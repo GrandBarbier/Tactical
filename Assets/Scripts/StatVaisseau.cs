d3fe2f485939180d0caba3a7185a2a3a4c91c981
Gryfs <Allan18040@gmail.com>
2020-04-27 16:04:04 +0200

    Scriptable Object

---
+++ Assets/Scripts/StatVaisseau.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Ship", menuName = "Ship")]
+public class StatVaisseau : ScriptableObject
+{
+    public int prix;
+    public int shield;
+    public int health;
+    public int mvt;
+    public int dmg;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
