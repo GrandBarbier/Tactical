c268f0701be20c15722fc9a3c14d9d0a9f6c15f9
Gryfs <Allan18040@gmail.com>
2020-04-29 17:13:20 +0200

    Ajour money

---
+++ Assets/Scripts/Money.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Money : MonoBehaviour
+{
+    public List<GameObject> Nbstation = new List<GameObject>();
+    public int argent;
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject station in GameObject.FindGameObjectsWithTag("StationP1"))
+        {
+            Nbstation.Add(station);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = argent.ToString();
+    }
+
+    public void LelioEstLong()
+    {
+        argent = argent + Nbstation.Count * 1500;
+    }
+}
