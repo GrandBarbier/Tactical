0fb038a0f296e8edcba38208ed8a5bb466f3fcd6
Gryfs <Allan18040@gmail.com>
2020-05-05 16:45:36 +0200

    Tentative fix

--- Assets/Scripts/Movements/MovementsTilemap.cs
+++ Assets/Scripts/Movements/MovementsTilemap.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Pathfinding;
-using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.Serialization;
 using UnityEngine.Tilemaps;
