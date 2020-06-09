e58b62661bbb33d8af9dc56a678ac5ff038013e3
Amaury Janneteau <amaury.janneteau@e-artsup.net>
2020-06-09 16:27:39 +0200

    Halo almost done

---
+++ Assets/Scripts/HaloHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HaloHandler : MonoBehaviour
+{
+    public Sprite available;
+
+    public Sprite moved;
+
+    public Sprite attacked;
+
+    private void Start()
+    {
+        GetComponentInChildren<Image>().enabled = false;
+    }
+
+    void Update()
+    {
+        GetComponentInChildren<Image>().sprite = available;
+        
+        if (GetComponent<Stats>().moved)
+        {
+            GetComponentInChildren<Image>().sprite = moved;
+            if (GetComponent<Stats>().attacked)
+            {
+                GetComponentInChildren<Image>().sprite = attacked;
+
+            }
+        }
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        GetComponentInChildren<Image>().enabled = true;
+    }
+
+    private void OnMouseExit()
+    {
+        GetComponentInChildren<Image>().enabled = false;
+    }
+}
