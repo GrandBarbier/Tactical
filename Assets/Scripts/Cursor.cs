d466b1a589399cc2172a0def7a1423cb3664e9d3
Lelio <lelio.rollin@e-artsup.net>
2020-06-11 17:08:41 +0200

    Cursor

---
+++ Assets/Scripts/Cursor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+    public Texture2D cursor;
+    public Texture2D click;
+    
+    void Start()
+    {
+        UnityEngine.Cursor.SetCursor(cursor,Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            UnityEngine.Cursor.SetCursor(click,Vector2.zero, CursorMode.ForceSoftware);
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            UnityEngine.Cursor.SetCursor(cursor,Vector2.zero, CursorMode.ForceSoftware);
+        }
+    }
+}
